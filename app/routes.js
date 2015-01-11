b213a568c22eca3c12aa6c6296b15586ca2cb882
Spencer <stank@dimagi.com>
2015-01-11 14:57:20 -0500

    signup

--- app/routes.js
+++ app/routes.js
@@ -281,18 +281,18 @@ module.exports = function(app, passport) {
     // SIGNUP ==============================
     // =====================================
     // show the signup form
-    // app.get('/signup', function(req, res) {
+    app.get('/signup', function(req, res) {
 
-    //     // render the page and pass in any flash data if it exists
-    //     res.render('signup.html', { message: req.flash('signupMessage') });
-    // });
+        // render the page and pass in any flash data if it exists
+        res.render('signup.html', { message: req.flash('signupMessage') });
+    });
 
-    // // process the signup form
-    // app.post('/signup', passport.authenticate('local-signup', {
-    //     successRedirect : '/admin', // redirect to the secure profile section
-    //     failureRedirect : '/signup', // redirect back to the signup page if there is an error
-    //     failureFlash : true // allow flash messages
-    // }));
+    // process the signup form
+    app.post('/signup', passport.authenticate('local-signup', {
+        successRedirect : '/admin', // redirect to the secure profile section
+        failureRedirect : '/signup', // redirect back to the signup page if there is an error
+        failureFlash : true // allow flash messages
+    }));
 
     app.get('/admin', function(req, res) {
         var user = req.user;
